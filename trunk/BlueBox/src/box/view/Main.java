007beb38dd9ae6a43a07858d53f8accc2d3ba8b8
moriator <moriator@89f0c41f-9f1c-0db1-6c8d-6200135e8b37>
2011-01-21 03:16:45 +0000

    Nang cap 21/01/10

---
+++ trunk/BlueBox/src/box/view/Main.java
@@ -0,0 +1,33 @@
+package box.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Main extends Activity{
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		Intent iInitPassword = new Intent(Main.this, InitPasswordView.class);
+		startActivityForResult(iInitPassword, 0);
+	}
+
+	// get result from InitPasswordView
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		super.onActivityResult(requestCode, resultCode, data);
+//
+//		if(resultCode == 0) //all done
+//		{
+//			this.finish();
+//		}
+//	}
+//	
+//	public boolean firstTime() {
+//		File file = new File ("pwd_md5");
+//		return !file.exists();
+//	}
+}
